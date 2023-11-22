4a658ce98ff0ed907ac9c030faf0d0e2ba385744
Masher-111 <90869108+Masher-111@users.noreply.github.com>
2023-11-22 20:48:47 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,15 +1,20 @@
 const http = require('http');
 
+let userCount = 0;
+
 const server = http.createServer((req, res) => {
-    console.log('Запрос получен');
-    if (req.url === '/home') {
+    userCount++;
+    if (req.url === '/') {
     res.writeHead(
         200,  {'Content-Type' : 'text/html; charset=UTF-8'});
-    res.end('<h1>Добро пожаловать на главную страницу!</h1>');
+    res.write(`<h4>Количество просмотров: ${userCount}</h4>`);
+    res.end('<a href="./about">Перейти на страницу обо мне!</a>');
+    
     } else if (req.url === '/about') {
         res.writeHead(
             200,  {'Content-Type' : 'text/html; charset=UTF-8'});
-        res.end('<h1>Добро пожаловать на страницу обо мне!</h1>');
+            res.write(`<h4>Количество просмотров: ${userCount}</h4>`);
+        res.end('<a href="/">Перейти на главную страницу!</a>');
         } else {
             res.writeHead(
                 404,  {'Content-Type' : 'text/html; charset=UTF-8'});
